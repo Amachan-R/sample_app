fbe31c9fb9c65d858be6d9917477cc4b862598fb
EC2 Default User <ec2-user@ip-172-31-32-13.ec2.internal>
2021-08-09 15:20:43 +0000

    [Finish] 2-5

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get 'top' => 'homes#top'
   post 'todolists' => 'todolists#create'
   get 'todolists' => 'todolists#index'
+  # .../todolists/1 や .../todolists/3 に該当する
+  get 'todolists/:id' => 'todolists#show', as: 'todolist'
+
 
 
 end
